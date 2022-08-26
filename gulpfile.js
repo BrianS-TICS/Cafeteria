7f2042d619deb6f5ca8d87043d748ace647991c2
BrianS-TICS <briansanchezaguilar@gmail.com>
2022-08-25 23:29:52 -0600

    update gulp config & scss config

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
-const {src, dest, watch, series, parallel} = require('gulp');
+const {src, dest, watch, series} = require('gulp');
+
+//CSS Y SASS
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+
+// IMAGENES
+const imagemin = require('gulp-imagemin');
+
 
 function css(done){
     // Compila sass
@@ -9,21 +15,30 @@ function css(done){
         // Compilar
         // OutputStyle te permite comprimir los elementos compilados (minifica)
         .pipe( sass() )
-        .pipe( postcss( [autoprefixer()] ) )
+        .pipe( postcss( [ autoprefixer() ] ) )
         // Guardar
         .pipe( dest('build/css') )
     // Termina la ejecucion de la funcion
-    done();
+        done();
+}
+
+function imagenes() {
+    return src('src/img/**/*')
+        .pipe( imagemin({ optimizationLevel : 3 }) )    
+        .pipe(dest('build/img'))
+        
 }
 
 function dev(){
     // Watch queda a la escucha de una archivo y una funcion
     // Siempre dejar al final en la ejecucucion
-    watch('src/scss/**/*.scss',css); // ** = Todas las carpetas | * = Todos los archivos 
+    watch('src/scss/**/*.scss', css); // ** = Todas las carpetas | * = Todos los archivos 
+    watch('src/img/**/*', imagenes);
 }
 
 exports.css = css; 
+exports.imagenes = imagenes;
 exports.dev = dev;
-exports.default = series(css, dev);
+exports.default = series(imagenes,css, dev);
 // Series : Inicia las tareas una a una - (Inicia una, termina y sigue la siguiente)
 // Parallel - Todas inician al mismo tiempo
